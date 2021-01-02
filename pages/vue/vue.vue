e5b5ba94c34a11bde83a5afbd856fa766777e395
dengjunyu668@163.com <dengjunyu668@163.com>
2021-01-02 17:56:47 +0800

    123

---
+++ pages/vue/vue.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		vue
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
